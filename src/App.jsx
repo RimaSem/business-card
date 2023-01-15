45278b7d4e7c2531e0e18a724348068c93ffa01e
RimaSem <rima.semetaite@gmail.com>
2023-01-15 16:13:34 +0200

    Create About and Interests components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Info from "./components/Info.jsx"
+import About from "./components/About.jsx"
+import Interests from "./components/Interests.jsx"
 
 export default function App() {
 
   return (
     <div className="App">
       <Info/>
+      <About/>
+      <Interests/>
     </div>
   )
 }
